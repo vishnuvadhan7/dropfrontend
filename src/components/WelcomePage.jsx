7a64f5148082a53901a701563236540edb01871e
vishnuvadhan7 <vishnuvadhan.alapat.i23@gmail.com>
2025-11-10 02:21:21 +0530

    Update WelcomePage.jsx

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Replace this with your actual image URL (or your uploaded image later)
-const illustrationUrl = '../../public/back.png';
+const illustrationUrl = 'back.png';
 
 const WelcomePage = ({ onGetStarted }) => {
   return (
@@ -45,3 +45,4 @@ const WelcomePage = ({ onGetStarted }) => {
 };
 
 export default WelcomePage;
+
